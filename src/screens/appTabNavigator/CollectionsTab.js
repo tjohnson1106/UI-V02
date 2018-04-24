64305aa9dc7ff391e3651dbf0c7993dc7db4b973
Thomas_Johnson <tjohnson1106@gmail.com>
2018-04-24 16:30:58 -0400

    created user profile thumbnail and username and description text

--- src/screens/appTabNavigator/CollectionsTab.js
+++ src/screens/appTabNavigator/CollectionsTab.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image } from "react-native";
 
-import { Icon, Container, Content, Header, Left, Right, Body } from "native-base";
+import { Icon, Container, Content, Header, Left, Right, Body, Button } from "native-base";
 
 class CollectionsTab extends Component {
   static navigationOptions = {
@@ -17,19 +17,52 @@ class CollectionsTab extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Collections Tab</Text>
-      </View>
+      <Container style={{ flex: 1, backgroundColor: "white" }}>
+        <Header>
+          <Left>
+            <Icon
+              name="settings"
+              ios="ios-settings-outline"
+              android="md-settings"
+              style={{ paddingLeft: 10 }}
+            />
+          </Left>
+          <Right>
+            <Icon
+              name="star"
+              ios="ios-star-outline"
+              android="md-star-outline"
+              type="Ionicons"
+              style={{ paddingRight: 10, fontSize: 32 }}
+            />
+          </Right>
+        </Header>
+        <Content>
+          <View>
+            <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+              <View style={{ flex: 1 }}>
+                <Image
+                  source={require("../../../assets/profile_one.jpeg")}
+                  style={{
+                    width: 75,
+                    height: 75,
+                    borderRadius: 37.5
+                  }}
+                />
+                <View style={{ flex: 1, justifyContent: "center" }}>
+                  <Text>Description</Text>
+                </View>
+              </View>
+              <View style={{ flex: 3 }}>
+                <Text>username</Text>
+                <Text>edit profile</Text>
+              </View>
+            </View>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
 
 export default CollectionsTab;
